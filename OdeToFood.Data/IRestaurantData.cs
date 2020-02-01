c4bba77d18a20aeeeb28d72c08b3defec56f4f44
akm0012 <akm0012@auburn.edu>
2020-02-01 12:48:13 -0500

    Made an Edit/create page. Completed Module 4.

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,9 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int Id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        int Commit();
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -36,6 +39,31 @@ namespace OdeToFood.Data
                 where string.IsNullOrEmpty(name) || restaurant.Name.StartsWith(name)
                 orderby restaurant.Name
                 select restaurant;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+
+            return restaurant;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
         }
     }
 }
--- OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -8,6 +8,12 @@ namespace OdeToFood.Pages.Restaurants
     public class Detail : PageModel
     {
         private readonly IRestaurantData restaurantData;
+
+        // Here we are binging some TempData. The spelling of "Message" is important, as that ths Key used to store 
+        // the data. (See Edit.cs model)
+        [TempData]
+        public string Message { get; set; }
+        
         public Restaurant Restaurant { get; set; }
 
         public Detail(IRestaurantData restaurantData)
